2180940523db1312d51761a0c91b62e57affd20b
Balkumar Vishwakarma <105841489+balkumarvishwakarma8090@users.noreply.github.com>
2022-05-22 17:05:00 +0530

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+        int m = 22, n = 44;// m and n are variable ,22 and 44 are constant
+        int a = 50, b = 80;// a and b are variables ,50 and 80 are constant
+
+        printf("m=%d\n",m);
+        printf("n=%d\n",n);
+        printf("a=%d\n",a);
+        printf("b=%d\n",b);
+        return 0;
+
+}
