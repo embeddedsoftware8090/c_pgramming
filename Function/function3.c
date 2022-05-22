4488b4fef6a0562aa1a881fe6e3c36abb5fbe366
Balkumar Vishwakarma <105841489+balkumarvishwakarma8090@users.noreply.github.com>
2022-05-22 17:19:23 +0530

    Create function3.c

---
+++ Function/function3.c
@@ -0,0 +1,16 @@
+//3.function with arguments and without return value
+#include<stdio.h>
+int sum(int, int);
+int main()
+{
+        int a,b,result;
+        printf("Enter two numbers:\n");
+        scanf("%d %d",&a,&b);
+        result = sum(a,b);
+        printf("The sum is : %d\n",result);
+}
+int sum(int a, int b)
+{
+        return a+b;
+}
+
