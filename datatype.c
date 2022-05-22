bf1271b460bb2d6bdba1e5a02048d1e32a78fd37
Balkumar Vishwakarma <105841489+balkumarvishwakarma8090@users.noreply.github.com>
2022-05-22 17:05:27 +0530

    Create datatype.c

---
+++ datatype.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+   int a;
+   char b;
+   float c;
+   double d;
+   printf("int size         = %d \n",sizeof(a));
+   printf("char size      = %d \n",sizeof(b));
+   printf("float size      = %d \n",sizeof(c));
+   printf("double size  = %d\n",sizeof(d));
+   return 0;
+}
