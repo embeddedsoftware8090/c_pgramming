bfdb3652964de55220c6c889ad22117eab76a58a
Balkumar Vishwakarma <105841489+balkumarvishwakarma8090@users.noreply.github.com>
2022-05-22 17:03:56 +0530

    Create identifier.c

---
+++ identifier.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+        int _univision = 10;//valid
+        int ABCD = 20;//valid
+        int _123 = 30;//valid
+        int bal123 = 12;//
+//         int 12univision = 20;//not valid
+        printf("%d\n",ABCD);
+        printf("%d\n",_123);
+        printf("%d\n",bal123);
+        printf("%d\n",_univision );
+//      printf("%d\n",12univision );
+        return 0;
+}
+
