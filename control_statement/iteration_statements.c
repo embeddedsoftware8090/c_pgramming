21a4a885e14a209ea35f215ee8f73f011c10a03d
Balkumar Vishwakarma <105841489+balkumarvishwakarma8090@users.noreply.github.com>
2022-05-22 17:15:11 +0530

    Create iteration_statements.c

---
+++ control_statement/iteration_statements.c
@@ -0,0 +1,22 @@
+//for(initialization;condition;updation)
+       //  for(i=0;;i++)  infinitive loop
+
+#include<stdio.h>  
+int main(){  
+int i=0;        
+for(i=1;i<=10;i++){      
+printf("%d \n",i);      
+}     
+return 0;  
+}
+While loop
+#include<stdio.h>  
+int main(){    
+int i=1;      
+while(i<=10){      
+printf("%d \n",i);      
+i++;      
+}  
+return 0;  
+}  
+
