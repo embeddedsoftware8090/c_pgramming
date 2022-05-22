ec818f6052a76e9d0cfd9c668db8407cce6568ce
Balkumar Vishwakarma <105841489+balkumarvishwakarma8090@users.noreply.github.com>
2022-05-22 17:09:33 +0530

    Create logical.c

---
+++ operators/logical.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main() {
+
+   int a = 5;
+   int b = 20;
+   int c ;
+
+   if ( a && b ) {
+      printf("Line 1 - Condition is true\n" );
+   }
+
+   if ( a || b ) {
+      printf("Line 2 - Condition is true\n" );
+   }
