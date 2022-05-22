84b26bdd0f82fae4170bbc331df080ab6098d04f
Balkumar Vishwakarma <105841489+balkumarvishwakarma8090@users.noreply.github.com>
2022-05-22 17:06:31 +0530

    Create assignment.c

---
+++ assignment.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+        int a = 20;//valid
+//      int 20 =b;//invalid
+        int b = 30;
+        int c = a+b;//valid
+        printf("%d %d %d\n",a,b,c);
+        return 0;
+}
