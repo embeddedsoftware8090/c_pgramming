12ed6318ab9b9c6eac1bca84c59693e6996542c5
Balkumar Vishwakarma <105841489+balkumarvishwakarma8090@users.noreply.github.com>
2022-05-22 17:12:04 +0530

    Create innbuilt_function.c

---
+++ innbuilt_function.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char A[50] = "univision technology bangalore";
+
+    int x;
+
+    x = strlen(A);//find the length of strings
+
+    printf("Length of A is %d \n",x);
+
+    return 0;
+}
